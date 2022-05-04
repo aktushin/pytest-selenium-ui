02f1550397ef605c711da829ae6436f5882e881d
unknown <aktushin.alex@gmail.com>
2022-05-04 23:51:10 +0700

    refactor, added flag --headless for run tests in headless mode

--- conftest.py
+++ conftest.py
@@ -4,23 +4,29 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome', help='Choose browser: chrome or firefox')
+    parser.addoption('--headless', action='store', default=True, help='Type True for activate headless mode')
 
 
 @pytest.fixture
 def driver(request):
-    browser_name = request.config.getoption('browser')
     driver = None
+    browser_name = request.config.getoption('browser')
+    headless_mode = request.config.getoption('headless')
 
     if browser_name == 'chrome':
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        options = ChromeOptions()
+        if headless_mode:
+            options.add_argument('--headless')
+        options.add_argument('--start-maximized')
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     elif browser_name == 'firefox':
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        driver.maximize_window()
 
     yield driver
 
