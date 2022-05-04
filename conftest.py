f5fd9808a13d43d274071616fd2d548280c410cc
unknown <aktushin.alex@gmail.com>
2022-05-04 18:54:35 +0700

    added requirements.txt, conftest.py

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture
+def driver(request):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    request.cls.dirver = driver
+
+    yield driver
+
+    # driver.quit()
