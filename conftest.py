f6f50f5943d666537ca38288d567cb98b471b5da
unknown <aktushin.alex@gmail.com>
2022-05-04 23:10:23 +0700

    added flag --browser for browser select (chrome or firefox)

--- conftest.py
+++ conftest.py
@@ -1,12 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome', help='Choose browser: chrome or firefox')
 
 
 @pytest.fixture
 def driver(request):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    request.driver = driver
+    browser_name = request.config.getoption('browser')
+    driver = None
+
+    if browser_name == 'chrome':
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser_name == 'firefox':
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
     yield driver
 
