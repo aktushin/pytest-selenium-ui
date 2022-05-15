91da37c677443ffb87716e8b6fc64f0f9e089f50
unknown <aktushin.alex@gmail.com>
2022-05-15 23:41:00 +0700

    added buttons page url in config

--- config/config.py
+++ config/config.py
@@ -20,6 +20,7 @@ CHECK_BOX_URL = 'https://demoqa.com/checkbox'
 LOGIN_URL = 'https://demoqa.com/login'
 RADIO_BUTTON_URL = 'https://demoqa.com/radio-button'
 WEB_TABLES_URL = 'https://demoqa.com/webtables'
+BUTTONS_URL = 'https://demoqa.com/buttons'
 
 USER_NAME = os.getenv('USER_NAME')
 PASSWORD = os.getenv('PASSWORD')
